147b68e31f369d947a573641a308e84bfe030375
rgb_beard <62804472+rgbbeard@users.noreply.github.com>
2021-05-03 15:21:34 +0200

    Update utils.php

--- utils.php
+++ utils.php
@@ -190,7 +190,7 @@ function get_time(): string {
     return date("G:i:s");
 }
 
-function obliviate_sql($target, $resize = {}) {
+function obliviate_sql($target, $resize = []) {
     $forbidden_words = ["select", "update", "where", "like", "delete", "alter", "date", "drop", "use", "or", "and", "not"];
 
     $target = preg_replace("/[\=|\;|\+|\-|\*|\)|\(|\%|\/]+/", "", $target);
